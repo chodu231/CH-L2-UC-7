00b63c8b841a131c0bf0c76be2d0a253da2f4da6
Cezary Hodun <chodu@softserveinc.com>
2023-07-06 12:22:28 +0200

    UC#7 Create unit tests

--- UC7Tests/StudentConverterTests.cs
+++ UC7Tests/StudentConverterTests.cs
@@ -1,10 +1,102 @@
-﻿using Xunit;
+﻿using MainApp;
+using Xunit;
 
 namespace UC7Tests
 {
     public class StudentConverterTests
     {
+        private StudentConverter _testSubject;
+
+        public StudentConverterTests()
+        {
+            _testSubject = new StudentConverter();
+        }
+
         [Fact]
-        public void ExampleTest() { }
+        public void ConvertStudents_HonorRollBecomesTrue()
+        {
+            // Arrange
+            var students = new List<Student>
+            {
+                new Student { Name = "Test1", Age = 25, Grade = 95 }
+            };
+
+            // Act
+            var result = _testSubject.ConvertStudents(students);
+
+            // Assert
+            Assert.True(result.First().HonorRoll);
+        }
+
+        [Fact]
+        public void ConvertStudents_ExceptionalBecomesTrue()
+        {
+            // Arrange
+            var students = new List<Student>
+            {
+                new Student { Name = "Test2", Age = 20, Grade = 95 }
+            };
+
+            // Act
+            var result = _testSubject.ConvertStudents(students);
+
+            // Assert
+            Assert.True(result.First().Exceptional);
+        }
+
+        [Fact]
+        public void ConvertStudents_PassedBecomesTrue()
+        {
+            // Arrange
+            var students = new List<Student>
+            {
+                new Student { Name = "Test3", Age = 22, Grade = 80 }
+            };
+
+            // Act
+            var result = _testSubject.ConvertStudents(students);
+
+            // Assert
+            Assert.True(result.First().Passed);
+        }
+
+        [Fact]
+        public void ConvertStudents_PassedRemainsFalse()
+        {
+            // Arrange
+            var students = new List<Student>
+            {
+                new Student { Name = "Test4", Age = 18, Grade = 60 }
+            };
+
+            // Act
+            var result = _testSubject.ConvertStudents(students);
+
+            // Assert
+            Assert.False(result.First().Passed);
+        }
+
+        [Fact]
+        public void ConvertStudents_EmptyListGiven()
+        {
+            // Arrange
+            var students = new List<Student>();
+
+            // Act
+            var result = _testSubject.ConvertStudents(students);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void ConvertStudents_InputIsNotAList()
+        {
+            // Arrange
+            List<Student> students = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => _testSubject.ConvertStudents(students));
+        }
     }
 }
